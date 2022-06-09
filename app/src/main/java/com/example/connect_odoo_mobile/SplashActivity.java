6609c701455e55353279751997955d547865681d
trwvux <truongvu6021@gmail.com>
2022-06-09 15:13:27 +0700

    feat(splash): add splash screen

---
+++ app/src/main/java/com/example/connect_odoo_mobile/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.connect_odoo_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,SignInActivity.class));
+            }
+        },3000);
+    }
+}
