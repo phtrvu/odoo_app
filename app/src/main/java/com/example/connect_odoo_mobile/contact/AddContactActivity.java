42c1bd765b5c679c1c69ea4e176ea2b770242fcc
trwvux <truongvu6021@gmail.com>
2022-07-06 11:03:01 +0700

    feat(add contact): choose image from the library

--- app/src/main/java/com/example/connect_odoo_mobile/contact/AddContactActivity.java
+++ app/src/main/java/com/example/connect_odoo_mobile/contact/AddContactActivity.java
@@ -1,24 +1,44 @@
 package com.example.connect_odoo_mobile.contact;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.connect_odoo_mobile.R;
+import com.example.connect_odoo_mobile.dialog.ChoosePictureDialog;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class AddContactActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+    private ImageView imgAvatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,21 @@ public class AddContactActivity extends AppCompatActivity {
         setToolbar();
         //checkIsCompany
         checkIsCompany();
+        //tap Image view
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tapImageView();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void tapImageView() {
+        imgAvatar = findViewById(R.id.imgAvatar);
+        imgAvatar.setOnClickListener(view -> {
+            ChoosePictureDialog choosePictureDialog = new ChoosePictureDialog(this);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                choosePictureDialog.openDialog();
+            }
+        });
     }
 
     private void setToolbar() {
@@ -71,4 +106,57 @@ public class AddContactActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        activityResultLauncher.launch(Intent.createChooser(intent, "Choose picture:"));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void checkPermission() {
+        int check = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (check == PackageManager.PERMISSION_GRANTED) {
+            openGallery();
+        } else {
+            String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permission, REQUEST_CODE);
+        }
+
+    }
+
+    public final ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == RESULT_OK) {
+                                Intent intent = result.getData();
+                                if (intent == null) {
+                                    return;
+                                }
+                                Uri uri = intent.getData();
+                                //Log.d("Run", "Uri: " + uri);
+                                //profileFragment.setUri(uri);
+                                try {
+                                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                                    //Log.d("Run", "bitmap: " + bitmap);
+                                    imgAvatar.setImageBitmap(bitmap);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
 }
---
+++ app/src/main/java/com/example/connect_odoo_mobile/dialog/ChoosePictureDialog.java
@@ -0,0 +1,66 @@
+package com.example.connect_odoo_mobile.dialog;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.example.connect_odoo_mobile.R;
+import com.example.connect_odoo_mobile.contact.AddContactActivity;
+
+public class ChoosePictureDialog {
+    private final AddContactActivity context;
+
+    public ChoosePictureDialog(AddContactActivity context) {
+        this.context = context;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void openDialog() {
+        Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_choose_picture);
+
+        Window window = dialog.getWindow();
+        if (window == null) {
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = Gravity.BOTTOM;
+        window.setAttributes(windowAttributes);
+        //event close dialog when tap out dialog
+        dialog.setCancelable(true);
+        //mapping
+        TextView txtCancel = dialog.findViewById(R.id.txtCancel);
+        TextView txtViewPicture = dialog.findViewById(R.id.txtViewPicture);
+        TextView txtLibrary = dialog.findViewById(R.id.txtLibrary);
+        TextView txtCamera = dialog.findViewById(R.id.txtCamera);
+        //event close dialog when tap cancel
+        txtCancel.setOnClickListener(view -> dialog.cancel());
+        //event choose picture from the library
+        txtLibrary.setOnClickListener(view -> {
+            dialog.cancel();
+            context.checkPermission();
+        });
+        //show dialog
+        dialog.show();
+    }
+
+}
